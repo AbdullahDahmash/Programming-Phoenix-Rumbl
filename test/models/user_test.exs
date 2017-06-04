a45aaf415ee9c054e30609788d3a0533c8b744f1
Abdullah <Abdullah.dahmash@yahoo.com>
2017-06-04 16:11:00 +0300

    added user model test

---
+++ test/models/user_test.exs
@@ -0,0 +1,22 @@
+defmodule Rumbl.UserTest do
+  use Rumbl.ModelCase, async: true
+  alias Rumbl.User
+
+  @valid_attrs %{name: "A User", username: "eva", password: "secret"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+
+  test "changeset does not accept long username" do
+    attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
+    assert errors_on(%User{}, attrs)[:username] == "should be at most 20 character(s)"
+  end
+end
