25417a21efe6a038812f39219b79bc940e9c5f35
Abdullah <Abdullah.dahmash@yahoo.com>
2017-06-04 16:35:55 +0300

    user tests

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -19,4 +19,20 @@ defmodule Rumbl.UserTest do
     attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
     assert errors_on(%User{}, attrs)[:username] == "should be at most 20 character(s)"
   end
+
+  test "registration_changeset password must be at least 6 chars" do
+    attrs = Map.put(@valid_attrs, :password, "12345")
+    changeset = User.registration_changeset(%User{}, attrs)
+    assert changeset.errors[:password] == {"should be at least %{count} character(s)", [count: 6, validation: :length, min: 6]}
+  end
+
+  test "registration_changeset with valid attributes hashes password" do
+    attrs = Map.put(@valid_attrs, :password, "123456")
+    changeset = User.registration_changeset(%User{}, attrs)
+    %{password: pass, password_hash: password_hash} = changeset.changes
+
+    assert changeset.valid?
+    assert password_hash
+    assert Comeonin.Bcrypt.checkpw(pass, password_hash)
+  end
 end
