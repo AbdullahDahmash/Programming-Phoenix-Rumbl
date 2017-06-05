e5a06684f282a9b62c01fd02db774b838872afc3
Abdullah <Abdullah.dahmash@yahoo.com>
2017-06-05 16:44:02 +0300

    added video channel

---
+++ web/static/js/video.js
@@ -0,0 +1,31 @@
+import Player from './player';
+
+let Video = {
+  init(socket, element) {
+    if (!element) {
+      return
+    }
+
+    let playerId = element.getAttribute("data-player-id");
+    let videoId = element.getAttribute("data-id");
+
+    socket.connect();
+
+    Player.init(element.id, playerId, () => {
+      this.onReady(videoId, socket);
+    });
+  },
+
+  onReady(videoId, socket) {
+    let msgContainer = document.getElementById("msg-container");
+    let msgInput = document.getElementById("msg-input");
+    let postButton = document.getElementById("msg-submit");
+    let vidChannel = socket.channel("videos:" + videoId);
+
+    vidChannel.join()
+      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("error", resp => console.log("join failed", reason))
+  }
+}
+
+export default Video;
