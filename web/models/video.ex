d784f9fff33c7aa8659c3f17a048e76222c44a61
Abdullah <Abdullah.dahmash@yahoo.com>
2017-06-05 15:05:21 +0300

    added slug to video

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,10 +1,12 @@
 defmodule Rumbl.Video do
   use Rumbl.Web, :model
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
 
   schema "videos" do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
 
     belongs_to :category, Rumbl.Category
@@ -19,6 +21,28 @@ defmodule Rumbl.Video do
     struct
     |> cast(params, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
+    |> slugify_title()
     |> assoc_constraint(:category)
   end
+
+  def slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+
+  defimpl Phoenix.Param, for: Rumbl.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
+  end
+
 end
