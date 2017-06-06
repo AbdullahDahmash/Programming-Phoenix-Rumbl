c6fb2a2ba6d3ef9a8b232195ea61cbfbd2928730
Abdullah <Abdullah.dahmash@yahoo.com>
2017-06-06 13:09:16 +0300

    send message using video channel

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -22,9 +22,48 @@ let Video = {
     let postButton = document.getElementById("msg-submit");
     let vidChannel = socket.channel("videos:" + videoId);
 
+    postButton.addEventListener("click", (e) => {
+      let payload = {
+        body: msgInput.value,
+        at: Player.getCurrentTime(),
+      };
+      vidChannel.push("new_annotation", payload)
+      .receive("error", (e) => console.log(e));
+
+      msgInput.value = "";
+
+    });
+
+    vidChannel.on("new_annotation", (resp) => {
+      this.renderAnnotation(msgContainer, resp);
+    });
+
+    vidChannel.on("ping", ({count}) => console.log("PING", count));
+
     vidChannel.join()
       .receive("ok", resp => console.log("joined the video channel", resp))
       .receive("error", resp => console.log("join failed", reason))
+  },
+
+  esc(str) {
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  },
+
+  renderAnnotation(msgContainer, { user, body, at }) {
+    let template = document.createElement("div");
+
+    template.innerHTML = `
+    <a href="#" data-seek="${this.esc(at)}">
+      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+    </a>
+    `
+
+    msgContainer.appendChild(template)
+
+    msgContainer.scrollTop = msgContainer.scrollHeight
+
   }
 }
 
