a06fcb9956fe6a15ed6967393d7aff2630cb0d8f
Abdullah Dahmash <abdullah.dahmash@yahoo.com>
2017-06-10 02:36:48 +0300

    added duplicate annotations when server dc

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -35,6 +35,8 @@ let Video = {
     });
 
     vidChannel.on("new_annotation", (resp) => {
+
+      vidChannel.params.last_seen_id = resp.id;
       this.renderAnnotation(msgContainer, resp);
     });
 
@@ -47,11 +49,13 @@ let Video = {
       if (!seconds) { return }
 
       Player.seekTo(seconds);
-    })
+    });
 
     vidChannel.join()
     .receive("ok", resp => {
-      this.scheduleMessages(msgContainer, resp.annotations)
+      let ids = resp.annotations.map((ann) => ann.id);
+      if (ids.length > 0) { vidChannel.params.last_seen_id = Math.max(...ids) }
+      this.scheduleMessages(msgContainer, resp.annotations);
       })
       .receive("error", reason => console.log("join failed", reason))
   },
