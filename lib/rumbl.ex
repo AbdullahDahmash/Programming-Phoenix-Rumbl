9323053730056e70db951c13dc22787ae1fdc6ed
Abdullah Dahmash <abdullah.dahmash@yahoo.com>
2017-06-10 19:21:30 +0300

    removed counter from main superviser

--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -13,13 +13,12 @@ defmodule Rumbl do
       # Start the endpoint when the application starts
       supervisor(Rumbl.Endpoint, []),
       # Start your own worker by calling: Rumbl.Worker.start_link(arg1, arg2, arg3)
-      worker(Rumbl.Counter, [5]),
       # worker(Rumbl.Worker, [arg1, arg2, arg3]),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_all, name: Rumbl.Supervisor]
+    opts = [strategy: :one_for_one, name: Rumbl.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
